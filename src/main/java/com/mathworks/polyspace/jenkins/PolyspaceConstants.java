65709eb5114759fe8d855c8a3d89639f22e4bf93
Stephane BOBIN <sbobin@mathworks.com>
2020-01-27 14:51:28 +0100

    Update copyright
    Signed-off-by: Stephane BOBIN <sbobin@mathworks.com>

--- src/main/java/com/mathworks/polyspace/jenkins/PolyspaceConstants.java
+++ src/main/java/com/mathworks/polyspace/jenkins/PolyspaceConstants.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The MathWorks, Inc.
+// Copyright (c) 2019-2020 The MathWorks, Inc.
 // All Rights Reserved.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
